1d3099abc181af22bb19eb425b6e58eaa8489c3d
Sun Lijun <ashther@qq.com>
2016-11-25 11:41:03 +0800

    add .Rd file with roxygen2

--- R/distanceCaculate.R
+++ R/distanceCaculate.R
@@ -1,10 +1,22 @@
+#' @title caculate distance
+#'
+#' @param lon_1,lon_2 numeric, degree longitude
+#' @param lat_1,lat_2 numeric, degree latitude
+#'
+#' @details
+#' caculate the spherical distance between 2 point on earth, although
+#' \code{geosphere::distHaversine} can caculate the spherical distance,
+#' \code{distanceCaculate} has about 10X speed than it and less than 0.2% error.
+#'
+#' @examples
+#' distanceCaculate(108.8821, 34.2341, 108.882, 34.2343)
 distanceCaculate <- function(lon_1, lat_1, lon_2, lat_2) {
   dx <- lon_1 - lon_2
   dy <- lat_1 - lat_2
-  
+
   b <- (lat_1 + lat_2) / 2
-  
+
   lx <- (0.9971 + 0.0004533 * b + -0.0001759 * b^2 + 0.0000005029 * b^3) * dx
-  
+
   sqrt(lx ^ 2 + dy ^ 2) * 111125
 }
--- R/geoVis.R
+++ R/geoVis.R
@@ -1,21 +1,34 @@
-
+#' @title visualize status
+#'
+#' @description visualize user's longitude and latitude on map
+#'
+#' @param df dataframe about users' geo information, including id, account_id,
+#' longitude, latitude and status field at least.
+#' @param ids integer vector, indicate the users whose status will be change to
+#' 1 and their cirlces on map will be blue.
+#'
+#' @details
+#' see users' status information on the map, the status field had already been
+#' settle by SQL store procedure on the remote server, if \code{ids} were
+#' provided, the circle represent ids will also been labeled as \code{status == 1}.
+#' blue circles indicate \code{status == 1}, and red ones indicate opposite.
 attenceVis <- function(df, ids = NULL) {
   require(dplyr)
   require(leaflet)
-  
+
   pal <- colorFactor(c("red", "blue"), domain = c(0, 1))
-  
+
   attence_id <- unique(df$id[df$status == 1])
   df$status[df$id %in% attence_id] <- 1
-  
+
   if (!is.null(ids)) {
     df$status[df$id %in% ids] <- 1
   }
-  
+
   df %>%
-    filter(!is.na(longitude) & !is.na(latitude)) %>% 
-    mutate(longitude = longitude - 0.011, 
-           latitude = latitude - 0.004) %>% 
+    filter(!is.na(longitude) & !is.na(latitude)) %>%
+    mutate(longitude = longitude - 0.011,
+           latitude = latitude - 0.004) %>%
     leaflet() %>%
     # setView(lng = median(df$longitude, na.rm = TRUE),
     #         lat = median(df$latitude, na.rm = TRUE),
